43aa4d60a05f5d07a2c1c4ca8f942e279c152b62
ayyubxon <ayyubxonhamdamovtatu@gmail.com>
2023-05-13 16:18:10 +0500

    r

--- blog/views.py
+++ blog/views.py
@@ -9,4 +9,7 @@ class UserView(APIView):
             return Response({ "username": user.username, "first_name": user.first_name, "last_name": user.first_name})
         except:
             return Response({'result':'User not found'})
+class CreateUserView(APIView):
+    def post(self,request,id:int):
+        
 
